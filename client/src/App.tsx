22a6d566b455616308481073cf10f523a5587286
KhaledAlkilani <khaledkelany@gmail.com>
2024-10-13 17:56:54 +0300

    added api root url, added one more GET request for list of movies and rendered it

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 
+interface Movies {
+  id: number;
+  name: string;
+}
+
 export default function App() {
   const [message, setMessage] = useState<string>("");
+  const [movies, setMovies] = useState<Movies[]>([]);
 
   useEffect(() => {
     fetch("http://localhost:5000/api/hello")
@@ -10,11 +16,24 @@ export default function App() {
       .catch((error) => console.error("Error fetching data", error));
   }, []);
 
+  useEffect(() => {
+    fetch("http://localhost:5000/api/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data.movies))
+      .catch((error) => console.error("Error fetching data", error));
+  }, []);
+
   return (
     <div>
       <h1 className="text-lg font-bold text-center">
         This is data from express.js server: {message}
       </h1>
+      <div className="flex flex-col p-5">
+        <h1>List of Movies</h1>
+        {movies.map((movie) => (
+          <li>{movie.name}</li>
+        ))}
+      </div>
     </div>
   );
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -11,8 +11,24 @@ app.use(
   })
 );
 
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the API");
+});
+
 app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello from Express!" });
+});
+
+const arrayOfObjectsMovies = [
+  { id: 1, name: "Scarface" },
+  { id: 2, name: "Training Day" },
+  { id: 3, name: "Knock  Knock" },
+  { id: 4, name: "Good Fellas" },
+];
+
+//---------------------
+app.get("/api/movies", (req: Request, res: Response) => {
+  res.json({ movies: arrayOfObjectsMovies });
 });
 
 app.listen(PORT, () => {
